0adf52a044d576195d58968b3dbb52f0ee6f607d
Thiago Dourado <thiagocfdourado@gmail.com>
2021-05-10 16:47:50 -0300

    Implementação do JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+if(window.SimpleSlide){
+    new SimpleSlide({
+        slide: 'quote', // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+      });
+    
+      new SimpleSlide({
+        slide: 'portfolio', // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        nav: true
+      });
+}
+
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
